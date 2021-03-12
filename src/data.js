7307244e55b6f6312a688946452b150e9614a47e
burakou_vitali <vitali.burakou@gmail.com>
2021-03-12 13:11:26 +0300

    init: shop amazona

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    {
+      _id: 1,
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d2.jpg',
+      brand: 'Nike',
+      price: 60,
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: 2,
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d2.jpg',
+      brand: 'Nike',
+      price: 50,
+      rating: 4.7,
+      numReviews: 3
+    },
+    {
+      _id: 3,
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d2.jpg',
+      brand: 'Nike',
+      price: 70,
+      rating: 3.4,
+      numReviews: 8
+    },
+    {
+      _id: 4,
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d2.jpg',
+      brand: 'Nike',
+      price: 70,
+      rating: 3.4,
+      numReviews: 8
+    },
+  ]
+}
+
+export default data
